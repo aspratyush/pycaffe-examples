1c56f999a6dfcf0a816ab2bbd117926e7a3ebd77
Pratyush <aspratyush@gmail.com>
2016-08-18 19:16:00 +0530

    Compare predictions and test data labels

--- assignments/plotting.sh
+++ assignments/plotting.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# code obtained from : https://aaronsplace.co.uk/blog/2015-12-22-caffe-log-plotting.html
 
 data=`cat solve.log | grep ", loss = " | awk '{ print $6,$9 }'`
 echo "$data" > solve1.log
--- assignments/prac2_solve.py
+++ assignments/prac2_solve.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 import caffe
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 #Load the net
 solver = caffe.get_solver("models/solver_prac2.prototxt")
@@ -23,6 +23,19 @@ inputData = np.array(
 [80, 32, 24]]
 )
 
+testData = np.array(
+[[40.32, 6, 4],
+[42.92, 10, 4],
+[45.33, 12, 5],
+[48.85, 14, 7],
+[52.37, 16, 9],
+[57, 18, 12],
+[61.82, 22, 14],
+[69.78, 24, 20],
+[72.19, 26, 21],
+[79.42, 32, 24]]
+)
+
 # Initialize memory data layer
 data = np.ascontiguousarray(np.float32(inputData[:,np.newaxis, np.newaxis, 1:]))
 predictions = np.ascontiguousarray(np.float32(inputData[:,0]))
@@ -32,3 +45,15 @@ solver.net.set_input_arrays(data, predictions)
 #solve
 solver.solve()
 
+print "----Performance on test data----"
+# Train data
+data = np.ascontiguousarray(np.float32(testData[:,np.newaxis, np.newaxis, 1:]))
+predictions = np.ascontiguousarray(np.float32(testData[:,0]))
+solver.net.set_input_arrays(data, predictions)
+
+#solve
+solver.net.forward()
+predictions = solver.net.blobs['predicted'].data
+for i in xrange(predictions.shape[0]):
+    print "Predicted = ", predictions[i,0], " .. Actual = ", testData[i,0], \
+            " .. Diff = ", np.abs(predictions[i,0] - testData[i,0])
